9c23ea65d8fc744f7d6a5eeb374c911567252200
arthur999999 <arthurhono.contato@gmail.com>
2024-11-27 15:24:34 -0300

    first commit

---
+++ programs/bonding-curve/src/lib.rs
@@ -0,0 +1,16 @@
+use anchor_lang::prelude::*;
+
+declare_id!("HvUE85QPy1VHfPXVR1DrQy3nEddHvApHy9btbLvH1FnX");
+
+#[program]
+pub mod bonding_curve {
+    use super::*;
+
+    pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
+        msg!("Greetings from: {:?}", ctx.program_id);
+        Ok(())
+    }
+}
+
+#[derive(Accounts)]
+pub struct Initialize {}
