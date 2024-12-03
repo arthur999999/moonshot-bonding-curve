8a08e0a606c60ea113bfe3fa44c6734815ff9a36
arthur999999 <arthurhono.contato@gmail.com>
2024-12-03 17:39:50 -0300

    finish test crate config account

--- tests/bonding-curve.ts
+++ tests/bonding-curve.ts
@@ -8,9 +8,44 @@ describe("bonding-curve", () => {
 
   const program = anchor.workspace.BondingCurve as Program<BondingCurve>;
 
-  it("Is initialized!", async () => {
-    // Add your test here.
-    const tx = await program.methods.initialize().rpc();
-    console.log("Your transaction signature", tx);
+  it("Test ConfigInit instruction", async () => {
+
+    const provider = anchor.getProvider();
+    const user = provider.publicKey;
+
+    const [configPda, bump] = await anchor.web3.PublicKey.findProgramAddress(
+      [Buffer.from("config_account")],
+      program.programId
+    );
+
+
+    const configParams = {
+      migrationAuthority: anchor.web3.Keypair.generate().publicKey,                   
+      backendAuthority: anchor.web3.Keypair.generate().publicKey, 
+      configAuthority: user,                  
+      helioFee: anchor.web3.Keypair.generate().publicKey,        
+      dexFee: anchor.web3.Keypair.generate().publicKey,                              
+      feeBps: 100,                             
+      dexFeeShare: 60,                            
+      migrationFee: new anchor.BN(4),                      
+      marketcapThreshold: new anchor.BN(345000000000),                  
+      marketcapCurrency: 0,                      
+      minSupportedDecimalPlaces: 6,             
+      maxSupportedDecimalPlaces: 9,             
+      minSupportedTokenSupply: new anchor.BN(10000000),       
+      maxSupportedTokenSupply: new anchor.BN(1000000000),    
+      coefB: 25,                                
+    };
+
+    const tx = await program.methods.configInit(configParams).accounts({
+      configAuthority: user,
+  
+    }).rpc();
+    console.log("Tx signature", tx);
+
+    let account = await program.account.configAccount.all();
+
+    console.log("Config account created", account);
+    
   });
 });
